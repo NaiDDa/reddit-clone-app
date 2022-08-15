813da0487ca726f5ef86f72bf45052f2109adae3
NaiDDa <77822025+NaiDDa@users.noreply.github.com>
2022-08-15 23:33:54 +0900

    commit

---
+++ server/src/entities/Entity.ts
@@ -0,0 +1,11 @@
+import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm";
+export default abstract class Entity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @CreateDateColumn()
+    updatedAt: Date;
+}
